f3316cdd3721803b73a70b3b86f1b37ff6ef4b1a
DangDan111 <dangdan0404@gmail.com>
2025-12-02 05:56:42 +0700

    sửa lỗi

--- thử 1.py
+++ thử 1.py
@@ -2,5 +2,5 @@ print('Quê hương là chùm khế ngọt')
 print('Cho con trèo hái mỗi ngày')
 print('Quê hương là đường đi học')
 print('Con về rợp bướm vàng bay')
-print('Quê hương mỗi người chỉ một')\
+print('Quê hương mỗi người chỉ một')
 print('Như là chỉ một mẹ thôi')
